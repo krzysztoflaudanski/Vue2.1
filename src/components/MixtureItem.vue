cfedd75354e821e2a9a0300165306ad90e5ee81b
Krzysztof <laudanski.krzysztof86@gmail.com>
2024-05-15 11:49:33 +0200

    add buttonitem

--- src/components/MixtureItem.vue
+++ src/components/MixtureItem.vue
@@ -1,30 +1,23 @@
 <template>
-  <li class="mixtures__mixture">
+<li class="mixtures__mixture">
 
-      <!-- decrement btn -->
-      <button
-        @click="$emit('decrement')"
-        class="mixtures__mixture__btn mixtures__mixture__btn--left">
-        <span
-          class="fas fa-arrow-down" />
-      </button>
+  <!-- decrement btn -->
+  <button-item class="mixtures__mixture__btn mixtures__mixture__btn--left" icon="arrow-down"
+    @click="$emit('decrement')" />
 
-      <div
-        :class="`mixtures__mixture__fill mixtures__mixture__fill--${mixture.variant}`"
-        :style="{ height: mixture.amount + '%' }" />
 
-      <!-- increment btn -->
-      <button
-        @click="$emit('increment')"
-        class="mixtures__mixture__btn mixtures__mixture__btn--right">
-        <span
-          class="fas fa-arrow-up" />
-      </button>
+  <div :class="`mixtures__mixture__fill mixtures__mixture__fill--${mixture.variant}`"
+    :style="{ height: mixture.amount + '%' }" />
 
-  </li>
+  <!-- increment btn -->
+  <button-item class="mixtures__mixture__btn mixtures__mixture__btn--right" icon="arrow-up" :movement="-0.5"
+    @click="$emit('increment')" />
+</li>
 </template>
 
 <script>
+import ButtonItem from './shared/ButtonItem.vue'
+
 export default {
   name: 'MixtureItem',
   props: {
@@ -32,12 +25,14 @@ export default {
       type: Object,
       required: true
     }
+  },
+  components: {
+    ButtonItem
   }
 }
 </script>
 
 <style scoped lang="scss">
-
 .mixtures__mixture {
   display: block;
   width: 10rem;
@@ -52,8 +47,8 @@ export default {
   overflow: hidden;
   background-color: #9a9a9a;
   background-image: linear-gradient(0deg, #9a9a9a 0%, #e8fdff 100%);
-  -webkit-box-shadow: 0 20px 40px 0 rgba(107,154,212,.5);
-  box-shadow: 0 20px 40px 0 rgba(107,154,212,.3);
+  -webkit-box-shadow: 0 20px 40px 0 rgba(107, 154, 212, .5);
+  box-shadow: 0 20px 40px 0 rgba(107, 154, 212, .3);
 
   &__fill {
     width: 100%;
@@ -74,40 +69,20 @@ export default {
       background-color: #25b6bf;
       background-image: linear-gradient(183deg, #25b6bf 0%, #0070a2 100%);
     }
-
   }
 
   &__btn {
-    background-color: #9a9a9a;
-    background-image: linear-gradient(0deg, #9a9a9a 0%, #e8fdff 100%);
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
-    width: 2rem;
-    height: 2rem;
-    border: none;
-    border-radius: 50%;
-    cursor: pointer;
-    -webkit-box-shadow: 0 20px 40px 0 rgba(107,154,212,.1);
-    box-shadow: 0 20px 40px 0 rgba(107,154,212,.1);
-    transition: .3s;
-    outline: none;
-    color: #637892;
 
     &--right {
       right: 1rem;
-      &:hover {
-        margin-top: -0.5rem;
-      }
     }
 
     &--left {
       left: 1rem;
-      &:hover {
-        margin-top: 0.5rem;
-      }
     }
   }
-
 }
 </style>
--- src/components/ResultBox.vue
+++ src/components/ResultBox.vue
@@ -1,27 +1,21 @@
 <template>
-  <div>
+<div>
 
-    <!-- text between -->
-    <p
-      v-text="'And the result...'" />
+  <!-- text between -->
+  <p v-text="'And the result...'" />
 
-    <!-- mixture effect -->
-    <big-mixture-item
-      :color="mixtureEffectFill" />
+  <!-- mixture effect -->
+  <big-mixture-item :color="mixtureEffectFill" />
 
-    <!-- refresh btn -->
-    <button
-      @click="$emit('refresh')"
-      class="refresh-btn">
-      <span
-        class="fas fa-sync" />
-    </button>
+  <!-- refresh btn -->
+  <button-item @click="$emit('refresh')" :size="4" :movement="-0.5" :font-size="1.5" icon="sync" />
 
-  </div>
+</div>
 </template>
 
 <script>
 import BigMixtureItem from './BigMixtureItem'
+import ButtonItem from './shared/ButtonItem.vue';
 
 export default {
   name: 'ResultsBox',
@@ -32,35 +26,14 @@ export default {
     }
   },
   computed: {
-    mixtureEffectFill () {
+    mixtureEffectFill() {
       const [redCol, greenCol, blueCol] = this.mixtures.map(item => Math.floor(item.amount * 2.5))
       return { 'background-color': `rgb(${redCol}, ${greenCol}, ${blueCol})` }
     }
   },
   components: {
-    BigMixtureItem
+    BigMixtureItem,
+    ButtonItem
   }
 }
 </script>
-
-<style scoped lang="scss">
-.refresh-btn {
-  background-color: #9a9a9a;
-  background-image: linear-gradient(0deg, #9a9a9a 0%, #e8fdff 100%);
-  width: 4rem;
-  height: 4rem;
-  border: none;
-  border-radius: 50%;
-  cursor: pointer;
-  -webkit-box-shadow: 0 20px 40px 0 rgba(107,154,212,.1);
-  box-shadow: 0 20px 40px 0 rgba(107,154,212,.1);
-  transition: .3s;
-  outline: none;
-  font-size: 1.5rem;
-  color: #637892;
-
-  &:hover {
-    margin-top: -0.5rem;
-  }
-}
-</style>
